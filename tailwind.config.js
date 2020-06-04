cced8b0c9a5c85bc644f4bcd539b88cc980ffcbe
Robin Malfait <malfait.robin@gmail.com>
2020-06-04 21:45:37 +0200

    add purge content paths
    Not really necessary, however I did test the application on my mobile
    device and the smaller bundle size made it a little bit nicer!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: [],
+  purge: ['./components/**/*.js', './pages/**/*.js'],
   theme: {
     extend: {
       fontFamily: {
